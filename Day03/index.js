cfb43049158e7d66c46753d3111ed280b6337d0d
Randall Roberts <randall@randallroberts.ca>
2020-12-03 20:05:04 -0500

    x-axis loops to left when out of bounds

--- Day03/index.js
+++ Day03/index.js
@@ -6,21 +6,27 @@ function openList(e) {
     const reader = new FileReader();
     reader.onload = (e) => {
         const list = e.target.result;
-        const rows = list.split('\n');
+        const rows = list.split('\r\n');
         
-        countTrees(rows);
+        countTrees(rows, 3, 1);
     };
     reader.readAsText(file);
 }
 
-function countTrees(treeMap) {
+function countTrees(treeMap, vx, vy) {
     trees = 0;
     x = 0;
     y = 0;
 
-    while (x < treeMap[0].length && y < treeMap.length) {
-        x += 3;
-        y += 1;
+    while (y < treeMap.length) {
+        x += vx;
+        if (x >= treeMap[0].length) {
+            x = x % treeMap[0].length;
+        }
+        y += vy;
+
+        console.log(x, y);
+
         //if we haven't left the map, check the location for a tree
         if (x < treeMap[0].length && y < treeMap.length) {
             if (treeMap[y][x] === '#') {
